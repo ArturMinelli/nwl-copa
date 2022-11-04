a22ed33ed375a597823afd2eee20de9cbc9c80ca
Artur Minelli Peixoto <arturminelli@gmail.com>
2022-11-04 09:50:32 -0300

    Server - Listing games

--- server/src/routes/game.ts
+++ server/src/routes/game.ts
@@ -1,5 +1,46 @@
+import { prisma } from "../lib/prisma";
 import { FastifyInstance } from "fastify";
+import { string, z } from "zod";
+import { authenticate } from "../plugins/authenticate";
 
 export async function gameRoutes(fastify: FastifyInstance) {
+  fastify.get(
+    '/pools/:id/games',
+    {
+      onRequest: [authenticate]
+    },
+    async (request, reply) => {
+      const listGamesParams = z.object({
+        id: string(),
+      })
 
+      const { id } = listGamesParams.parse(request.params)
+
+      const games = await prisma.game.findMany({
+        orderBy: {
+          date: 'desc'
+        },
+        include: {
+          guesses: {
+            where: {
+              participant: {
+                userId: request.user.sub,
+                poolId: id
+              }
+            }
+          }
+        }
+      })
+
+      return {
+        games: games.map((game) => {
+          return {
+            ...games,
+            guess: game.guesses.length > 0 ? game.guesses[0] : null,
+            guesses: undefined,
+          }
+        })
+      }
+    }
+  )
 }
