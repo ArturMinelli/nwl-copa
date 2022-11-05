a0e411f762c5e580a949d4861795420667fad77a
Artur Minelli Peixoto <arturminelli@gmail.com>
2022-11-05 07:16:01 -0300

    Mobile - Creating pool from mobile application

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,12 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { Box } from 'native-base'
 import { useAuth } from '../hooks/useAuth'
-import { SignIn } from '../screens/SignIn'
 import { AppRoutes } from './app.routes'
+import { SignIn } from '../screens/SignIn'
 
 export function Routes() {
   const { user } = useAuth()
-  console.log(user.name)
+  console.log(user)
   return (
     <Box flex={1} bgColor="gray.900">
       <NavigationContainer>
--- mobile/src/screens/NewPool.tsx
+++ mobile/src/screens/NewPool.tsx
@@ -1,10 +1,53 @@
-import { Heading, Text, VStack } from "native-base";
+import { Heading, Text, useToast, VStack } from "native-base";
 import { Header } from "../components/Header";
 import Logo from '../assets/nlw-copa-logo.svg'
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { api } from "../lib/api";
+import { useState } from "react";
+import { Alert } from "react-native";
 
 export function NewPool() {
+  const [poolTitle, setPoolTitle] = useState<string>('')
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const toast = useToast()
+
+  async function handleCreatePool() {
+    if(!poolTitle.trim()) {
+      return toast.show({
+        title: "Informe um nome para seu bolão!",
+        placement: 'top',
+        bgColor: 'red.500',
+      })
+    }
+
+    try {
+      setIsLoading(true)
+      await api.post('/pools', {
+        title: poolTitle,
+      })
+    }
+    catch (err) {
+      console.log(err)
+
+      toast.show({
+        title: "Não foi possível criar seu bolão!",
+        placement: 'top',
+        bgColor: 'red.500',
+      })
+    }
+    finally {
+      setPoolTitle('')
+      setIsLoading(false)
+
+      toast.show({
+        title: "Bolão criado com sucesso!",
+        placement: 'top',
+        bgColor: 'green.500',
+      })
+    }
+  }
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Criar novo bolão"/>
@@ -17,11 +60,15 @@ export function NewPool() {
 
         <Input
           mb={2}
+          onChangeText={setPoolTitle}
+          value={poolTitle}
           placeholder="Qual o nome do seu bolão?"
         />
 
         <Button
           title="Cirar meu bolão"
+          onPress={handleCreatePool}
+          isLoading={isLoading}
         />
         <Text color="gray.200" fontSize="sm" textAlign="center" px={10} mt={4}>
           Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas.
